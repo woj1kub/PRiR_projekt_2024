9d7e6ff91d71f405df27057db006360fa8a07492
wojkub <wk303204@student.polsl.pl>
2024-07-25 17:54:48 +0200

    testy muzyki

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 int main() {
     
     // Initialization
-    int screenWidth = 800;
-    int screenHeight = 450;
+    int screenWidth = 1600;
+    int screenHeight = 900;
 
     raylib::Color textColor(LIGHTGRAY);
     raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+
+    InitAudioDevice();
+
+    Sound bgMusic = LoadSound("assets/audio/chipi.wav"); 
+    PlaySound(bgMusic);
     
     SetTargetFPS(60);
 
@@ -24,6 +29,9 @@ int main() {
         textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
         EndDrawing();
     }
+
+    UnloadSound(bgMusic);
+    CloseAudioDevice();
 
     return 0;
 }
