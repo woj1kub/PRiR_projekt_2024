724b2c642457c19788de2d6d0110f41b662e9f40
MacWielgosz <92044905+MacWielgosz@users.noreply.github.com>
2024-07-28 16:26:05 +0200

    Dodanie rysowanie domów oraz sklepów

--- src/headers/game.h
+++ src/headers/game.h
@@ -1,16 +1,42 @@
-enum buildingType {
-    empty,
-    shop,
-    road,
-    home  
-};
-
-class Building {
+#define CellSize 50
+class Building { // To jest traktowane jako pusta 
+    protected:
+        Color _color = { 255, 203, 0, 50 };  
+    public:
+        void drawBuilding(short x , short y){
+            DrawRectangleLines(x * CellSize , y * CellSize , CellSize , CellSize , BLACK);
+        }
 };
 
 class Shop : Building {
+    public:
+        void drawBuilding(short x, short y){
+            Building::drawBuilding(x,y);
+            DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
+            DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
+            int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
+            int centerY = (y*CellSize) + 5 + (CellSize - 10) / 2;
+
+            DrawCircleLines(centerX, centerY,(CellSize/2)-10 , BLACK);
+        }
 };
 
+class Home : Building {
+    public:
+        void drawBuilding(short x, short y){
+            Building::drawBuilding(x,y);
+            DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
+            DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
+            int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
+
+            DrawLine(centerX, (y * CellSize) + 5, centerX, (y * CellSize) + 5 + CellSize - 10, BLACK);
+        }
+};
+
+class Road : Building {
+    private:
+        
+};
 
 class GameCell
 {
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include "headers/game.h"
 
-
 int main() {
-    
     // Initialization
     int screenWidth = 1600;
     int screenHeight = 900;
@@ -17,20 +15,30 @@ int main() {
     Sound bgMusic = LoadSound("assets/audio/chipi.wav"); 
     PlaySound(bgMusic);
     
-    SetTargetFPS(60);
+    Camera2D camera = { 0 };
+    camera.target = { screenWidth / 2.0f,screenHeight / 2.0f }; // Jest to tak ustawione aby był kwadrat 0,0 w górnym lewym rogu 
+    camera.offset = { screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
-        
+    SetTargetFPS(60);
 
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
-
         // TODO: Update your variables here
 
         // Draw
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
+            BeginMode2D(camera);
+                Shop build = Shop();
+                build.drawBuilding(0,0);
+                Home build1 = Home();
+                build1.drawBuilding(1,0);
+                
+            EndMode2D();
         EndDrawing();
     }
 
