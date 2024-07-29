95c0780268318ec54af4a6637ca04b0c8b6c0044
MacWielgosz <92044905+MacWielgosz@users.noreply.github.com>
2024-07-29 20:03:54 +0200

    Dodanie rysowanie planszy oraz dróg.

--- src/headers/game.h
+++ src/headers/game.h
@@ -5,14 +5,14 @@ class Building { // To jest traktowane jako pusta
     protected:
         Color _color = { 255, 203, 0, 50 };  
     public:
-        void drawBuilding(short x , short y){
+        virtual void drawBuilding(short x , short y){
             DrawRectangleLines(x * CellSize , y * CellSize , CellSize , CellSize , BLACK);
         }
 };
 
-class Shop : Building {
+class Shop : public Building {
     public:
-        void drawBuilding(short x, short y){
+        void drawBuilding(short x, short y) override {
             Building::drawBuilding(x,y);
             DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
             DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
@@ -23,9 +23,9 @@ class Shop : Building {
         }
 };
 
-class Home : Building {
+class Home : public Building {
     public:
-        void drawBuilding(short x, short y){
+        void drawBuilding(short x, short y) override {
             Building::drawBuilding(x,y);
             DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
             DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
@@ -35,9 +35,31 @@ class Home : Building {
         }
 };
 
-class Road : Building {
-    private:
-        
+class Road : public Building {
+private:
+    char roads;
+public:
+    Road(char roads) : roads(roads) {}
+
+    void drawBuilding(short x, short y) override {
+        Building::drawBuilding(x, y);
+        int centerX = (x * CellSize) + 5 + (CellSize - 10) / 2;
+        int centerY = (y * CellSize) + 5 + (CellSize - 10) / 2;
+
+        // Rysowanie w zależności od bitów w zmiennej 'roads'
+        if (roads & 0x01) { // pierwszy bit
+            DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize))}, 3.0f, BLACK); // do góry
+        }
+        if (roads & 0x02) { // drugi bit
+            DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize) + CellSize )}, 3.0f, BLACK); // w dół
+        }
+        if (roads & 0x04) { // trzeci bit
+            DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize)), (float)centerY}, 3.0f, BLACK); // w lewo
+        }
+        if (roads & 0x08) { // czwarty bit
+            DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize) + CellSize), (float)centerY}, 3.0f, BLACK); // w prawo
+        }
+    }
 };
 
 class GameCell
@@ -45,7 +67,27 @@ class GameCell
     public:
         short posX;
         short posY;
-        Building building;
+        Building* building;
 
         bool hasBuilding();
+        GameCell(short X, short Y){
+            posX = X;
+            posY=Y;
+            building = new Building();
+        }
+        void drawCell(){
+            building->drawBuilding(posX,posY);
+        }
+        void setHome(){
+            delete building;
+            building = new Home();
+        }
+        void setShop(){
+            delete building;
+            building = new Shop();
+        }
+        void setRoad(char roads){
+            delete building;
+            building = new Road(roads);
+        }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <raylib-cpp.hpp>
 #include <vector>
 #include "headers/game.h"
-
+using namespace std;
 int main() {
     // Initialization
     int screenWidth = 1600;
@@ -21,9 +21,25 @@ int main() {
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
+    size_t rows = 20;
+    size_t columns = 15;
+    vector<GameCell> map;
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            map.push_back(GameCell(i,j));
+        }   
+    }
+    map[10].setHome();
+    map[11].setShop();
+    map[12].setRoad(0b0001);
+    map[13].setRoad(0b0011);
+    map[14].setRoad(0b0111);
+    map[15].setRoad(0b1111);
+   
     SetTargetFPS(60);
-
-    // Main game loop
+     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
@@ -33,10 +49,11 @@ int main() {
         BeginDrawing();
             ClearBackground(RAYWHITE);
             BeginMode2D(camera);
-                Shop build = Shop();
-                build.drawBuilding(0,0);
-                Home build1 = Home();
-                build1.drawBuilding(1,0);
+
+                for (auto &&i : map)
+                {
+                    i.drawCell();
+                }
                 
             EndMode2D();
         EndDrawing();
