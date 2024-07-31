04651e0d020db3e33459ad1a7845618dd1e9d43e
MacWielgosz <92044905+MacWielgosz@users.noreply.github.com>
2024-07-31 19:13:08 +0200

    Podzielenie rysowania

--- src/headers/game.h
+++ src/headers/game.h
@@ -5,34 +5,17 @@ class Building { // To jest traktowane jako pusta
     protected:
         Color _color = { 255, 203, 0, 50 };  
     public:
-        virtual void drawBuilding(short x , short y){
-            DrawRectangleLines(x * CellSize , y * CellSize , CellSize , CellSize , BLACK);
-        }
+        virtual void drawBuilding(short x , short y);
 };
 
 class Shop : public Building {
     public:
-        void drawBuilding(short x, short y) override {
-            Building::drawBuilding(x,y);
-            DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
-            DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
-            int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
-            int centerY = (y*CellSize) + 5 + (CellSize - 10) / 2;
-
-            DrawCircleLines(centerX, centerY,(CellSize/2)-10 , BLACK);
-        }
+        void drawBuilding(short x, short y) override ;
 };
 
 class Home : public Building {
     public:
-        void drawBuilding(short x, short y) override {
-            Building::drawBuilding(x,y);
-            DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
-            DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
-            int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
-
-            DrawLine(centerX, (y * CellSize) + 5, centerX, (y * CellSize) + 5 + CellSize - 10, BLACK);
-        }
+        void drawBuilding(short x, short y) override ;
 };
 
 class Road : public Building {
@@ -41,25 +24,7 @@ private:
 public:
     Road(char roads) : roads(roads) {}
 
-    void drawBuilding(short x, short y) override {
-        Building::drawBuilding(x, y);
-        int centerX = (x * CellSize) + 5 + (CellSize - 10) / 2;
-        int centerY = (y * CellSize) + 5 + (CellSize - 10) / 2;
-
-        // Rysowanie w zależności od bitów w zmiennej 'roads'
-        if (roads & 0x01) { // pierwszy bit
-            DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize))}, 3.0f, BLACK); // do góry
-        }
-        if (roads & 0x02) { // drugi bit
-            DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize) + CellSize )}, 3.0f, BLACK); // w dół
-        }
-        if (roads & 0x04) { // trzeci bit
-            DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize)), (float)centerY}, 3.0f, BLACK); // w lewo
-        }
-        if (roads & 0x08) { // czwarty bit
-            DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize) + CellSize), (float)centerY}, 3.0f, BLACK); // w prawo
-        }
-    }
+    void drawBuilding(short x, short y) override ;
 };
 
 class GameCell
@@ -70,7 +35,7 @@ class GameCell
         Building* building;
 
         bool hasBuilding();
-        GameCell(short X, short Y){
+        GameCell(short X, short Y) {
             posX = X;
             posY=Y;
             building = new Building();
--- src/src/game.cpp
+++ src/src/game.cpp
@@ -3,3 +3,45 @@
 bool GameCell::hasBuilding() {
     return 0;
 }
+
+void Building::drawBuilding(short x , short y){
+    DrawRectangleLines(x * CellSize , y * CellSize , CellSize , CellSize , BLACK);
+}
+
+void Shop::drawBuilding(short x , short y) {
+    Building::drawBuilding(x,y);
+    DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
+    DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
+    int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
+    int centerY = (y*CellSize) + 5 + (CellSize - 10) / 2;
+
+    DrawCircleLines(centerX, centerY,(CellSize/2)-10 , BLACK);
+}
+
+void Home::drawBuilding(short x , short y){
+    Building::drawBuilding(x,y);
+    DrawRectangleLines((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , BLACK);
+    DrawRectangle((x * CellSize) + 5 , (y * CellSize) + 5 , CellSize-10 , CellSize-10 , _color);
+    int centerX = (x*CellSize) + 5 + (CellSize - 10) / 2;
+
+    DrawLine(centerX, (y * CellSize) + 5, centerX, (y * CellSize) + 5 + CellSize - 10, BLACK);
+}
+void Road::drawBuilding(short x , short y) {
+    Building::drawBuilding(x, y);
+    int centerX = (x * CellSize) + 5 + (CellSize - 10) / 2;
+    int centerY = (y * CellSize) + 5 + (CellSize - 10) / 2;
+
+    // Rysowanie w zależności od bitów w zmiennej 'roads'
+    if (roads & 0x01) { // pierwszy bit
+        DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize))}, 3.0f, BLACK); // do góry
+    }
+    if (roads & 0x02) { // drugi bit
+        DrawLineEx({(float)centerX, (float)centerY}, {(float)centerX, (float)((y * CellSize) + CellSize )}, 3.0f, BLACK); // w dół
+    }
+    if (roads & 0x04) { // trzeci bit
+        DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize)), (float)centerY}, 3.0f, BLACK); // w lewo
+    }
+    if (roads & 0x08) { // czwarty bit
+        DrawLineEx({(float)centerX, (float)centerY}, {(float)((x * CellSize) + CellSize), (float)centerY}, 3.0f, BLACK); // w prawo
+    }
+}
