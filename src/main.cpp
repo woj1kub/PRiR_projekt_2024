d71b63aec99ad4fcdebd185919cdabe11e1b2a2c
MacWielgosz <92044905+MacWielgosz@users.noreply.github.com>
2024-08-03 13:44:05 +0200

    Dodanie możliwość klikania na pole

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include <raylib-cpp.hpp>
 #include <vector>
 #include "headers/game.h"
+#include <iostream>
+#include <thread>
+#include <atomic>
+
 using namespace std;
+// Zmienne globalne ~ nazwy tymczasowe
+atomic<bool> running(true);
+mutex mapLock;
+
+size_t rows = 20;
+size_t columns = 15;
+
+vector<GameCell> map;
+
+float offSetTargetWidth = -300;
+float offSetTargetHeight = -50;
+
+int screenWidth = 1600;
+int screenHeight = 900;
+
+void logic()
+{
+    while (running.load())
+    {
+        lock_guard<mutex> guard(mapLock);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 mousePosition = GetMousePosition();
+            int pos = ((int)(mousePosition.y + offSetTargetHeight) / CellSize) + (columns * ((int)(mousePosition.x + offSetTargetWidth) / CellSize));
+            map[pos].setHome();
+            cout << mousePosition.x << endl;
+            cout << mousePosition.y << endl;
+        }
+        
+
+        // Tutaj dodać sprawdzanie sąsiadów dla dróg. Można to zrobić jeśli jest aktulizowana "map"
+    }
+}
+
 int main()
 {
     // Initialization
-    int screenWidth = 1600;
-    int screenHeight = 900;
 
     raylib::Color textColor(LIGHTGRAY);
     raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
@@ -17,14 +53,11 @@ int main()
     PlaySound(bgMusic);
 
     Camera2D camera = {0};
-    camera.target = {screenWidth / 2.0f, screenHeight / 2.0f}; // Jest to tak ustawione aby był kwadrat 0,0 w górnym lewym rogu
-    camera.offset = {screenWidth / 2.0f + 300, screenHeight / 2.0f + 50};
+    camera.target = {screenWidth / 2.0f + offSetTargetWidth, screenHeight / 2.0f + offSetTargetHeight};
+    camera.offset = {screenWidth / 2.0f, screenHeight / 2.0f};
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    size_t rows = 20;
-    size_t columns = 15;
-    vector<GameCell> map;
     for (size_t i = 0; i < rows; i++)
     {
         for (size_t j = 0; j < columns; j++)
@@ -32,6 +65,7 @@ int main()
             map.push_back(GameCell(i, j));
         }
     }
+
     map[10].setHome();
     map[11].setShop();
     map[12].setRoad(0b0001);
@@ -41,26 +75,27 @@ int main()
     map[16].setRoad(0b0);
 
     SetTargetFPS(60);
+    thread logicThread(logic);
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
-        // Update
-        // TODO: Update your variables here
-
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
         BeginMode2D(camera);
-
-        for (auto &&i : map) // To można zostawić
         {
-            i.drawCell();
-        }
+            lock_guard<mutex> guard(mapLock);
 
+            for (auto &&i : map) // To można zostawić
+            {
+                i.drawCell();
+            }
+        }
         EndMode2D();
         EndDrawing();
     }
-
+    running.store(false);
+    logicThread.join();
     UnloadSound(bgMusic);
     CloseAudioDevice();
 
